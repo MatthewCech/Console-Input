171ef2c07d70693600e35d9bd1bf80795ae62478
Matthew <contact@matthewcech.com>
2017-05-04 23:19:36 -0700

    = Merged to maintain comments and keycodes

--- Source/console-input.h
+++ Source/console-input.h
@@ -1,7 +1,7 @@
 /*!**********************************************************************
 @file    console-input.h
 @author  rwmc
-@date    4/6/2016
+@date    5/4/2016
 
 @brief 
 A wrapper allowing _getch and _kbhit windows-style functionality on 
@@ -20,9 +20,10 @@ both windows and linux! Relevant functions are KeyHit() and GetKey().
   /////////////////////
  // Keycode Defines //
 /////////////////////
-// You can compare output to characters mostly, however some
-// keys are a bit harder to type - I've throw a few keycodes
-// here, although note that accuracy may vary between systems.
+// Accuracy not guarenteed. Use ASCII characters when possible instead.
+// Keycodes based on ASCII Values, and ASCII Scan codes.
+
+// Main board keys
 #define KEY_BACKSPACE 8
 #define KEY_TAB       9
 #define KEY_ENTER     13
@@ -30,17 +31,47 @@ both windows and linux! Relevant functions are KeyHit() and GetKey().
 #define KEY_ESCAPE    27
 #define KEY_SPACE     32
 
-// ASCII readable but inconvenient sometimes.
-#define KEY_NUM_0 48
-#define KEY_NUM_1 49
-#define KEY_NUM_2 50
-#define KEY_NUM_3 51
-#define KEY_NUM_4 52
-#define KEY_NUM_5 53
-#define KEY_NUM_6 54
-#define KEY_NUM_7 55
-#define KEY_NUM_8 56
-#define KEY_NUM_9 57
+// Arrow Keys
+#define KEY_LEFT      75 // WARNING: Also capital K
+#define KEY_UP        72 // WARNING: Also capital H
+#define KEY_DOWN      80 // WARNING: Also capital P
+#define KEY_RIGHT     77 // WARNING: Also capital M
+
+// Utility Keys:
+#define KEY_INSERT    82 // WARNING: Also capital R
+#define KEY_DELETE    83 // WARNING: Also capital S
+#define KEY_HOME      71 // WARNING: Also capital G
+#define KEY_END       79 // WARNING: Also captial O
+#define KEY_PAGEUP    73 // WARNING: Also capital I
+#define KEY_PAGEDOWN  81 // WARNING: Also captial Q
+
+// Could not identify
+//#define KEY_PRINT_SCREEN 43, when CTRL is pressed it should be 114
+//#define KEY_SCROLL_LOCK  ??
+//#define KEY_PAUSE        ??
+//#define KEY_BREAK <would be same as pause>
+
+// Number Input (both numpad and primary keyboard)
+#define KEY_0         48
+#define KEY_1         49
+#define KEY_2         50
+#define KEY_3         51
+#define KEY_4         52
+#define KEY_5         53
+#define KEY_6         54
+#define KEY_7         55
+#define KEY_8         56
+#define KEY_9         57
+#define KEY_NUM_0     48
+#define KEY_NUM_1     49
+#define KEY_NUM_2     50
+#define KEY_NUM_3     51
+#define KEY_NUM_4     52
+#define KEY_NUM_5     53
+#define KEY_NUM_6     54
+#define KEY_NUM_7     55
+#define KEY_NUM_8     56
+#define KEY_NUM_9     57
 
 
 
@@ -151,4 +182,3 @@ inline int GetChar(void)
 }
 
 #endif // OS_NON_WINDOWS
-
